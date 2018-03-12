d92b917b48f0e0748ab6522e4907f5ab177c9db3
Colleen O'Reilly <colleen@jelcreative.com>
2018-03-12 17:56:08 -0400

    commented out waypoint

--- script.js
+++ script.js
@@ -5,15 +5,15 @@ $(".section-two-block").paroller({ factor: '0.3', type: 'background', direction:
 $(".column-one").paroller({ factor: '-0.1', type: 'foreground', direction: 'horizontal' });
 $(".column-three").paroller({ factor: '0.1', type: 'foreground', direction: 'horizontal' });
 
-var waypoint = $('.section-two-block').waypoint(function(direction) {
-              if (direction == 'down') {
-                $('.section-one-block').addClass('hidden')
-                console.log('down')
-              }
-              else {
-                $('.section-one-block').removeClass('hidden')
-                console.log('up')
-              }},
-              {
-                offset: '-80px'
-            })
+// var waypoint = $('.section-two-block').waypoint(function(direction) {
+//               if (direction == 'down') {
+//                 $('.section-one-block').addClass('hidden')
+//                 console.log('down')
+//               }
+//               else {
+//                 $('.section-one-block').removeClass('hidden')
+//                 console.log('up')
+//               }},
+//               {
+//                 offset: '-80px'
+//             })
