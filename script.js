6d9f6d173d516cc9c0b5b94b46cc63df8cda62cf
Colleen O'Reilly <colleen@jelcreative.com>
2018-03-12 19:26:36 -0400

    testing without paroller

--- script.js
+++ script.js
@@ -1,9 +1,27 @@
 console.log('linked')
 
-$('.two-text').paroller({ factor: '0.3', type: 'foreground', direction: 'vertical' });
+// $('.section-two-block').paroller({ factor: '0.3', type: 'background', direction: 'vertical' });
 
-$(".column-one").paroller({ factor: '-0.1', type: 'foreground', direction: 'vertical' });
-$(".column-three").paroller({ factor: '0.1', type: 'foreground', direction: 'vertical' });
+// $(".column-one").paroller({ factor: '-0.1', type: 'foreground', direction: 'vertical' });
+// $(".column-three").paroller({ factor: '0.1', type: 'foreground', direction: 'vertical' });
+
+
+$(window).on('scroll',function(){
+    parallaxScroll();
+});
+
+function parallaxScroll(){
+    var scrolled = $(window).scrollTop();
+    var faster = 0-(scrolled*.15)
+    $('.section-two-block').css('background-position-y', faster+'px');
+    var one = -scrolled * .08
+    var three = scrolled * .08
+    // $('.two-text').css('transform','translateY('+faster+'px)');
+    // if (scrolled > 1000) {
+  $('.column-one').css('transform','translateY('+one+'px)');
+  $('.column-three').css('transform','translateY('+three+'px)');
+// }
+}
 
 var waypoint = $('.section-two-block').waypoint(function(direction) {
               if (direction == 'down') {
