c7e8e7c635ef5b3966d4df4369beab927ed272b5
Colleen O'Reilly <colleen@jelcreative.com>
2018-03-12 18:01:43 -0400

    updated scroll effects

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
 console.log('linked')
 
-$(".section-two-block").paroller({ factor: '0.3', type: 'background', direction: 'vertical' });
+$('.two-text').paroller({ factor: '0.3', type: 'foreground', direction: 'vertical' });
 
-$(".column-one").paroller({ factor: '-0.1', type: 'foreground', direction: 'horizontal' });
-$(".column-three").paroller({ factor: '0.1', type: 'foreground', direction: 'horizontal' });
+$(".column-one").paroller({ factor: '-0.1', type: 'foreground', direction: 'vertical' });
+$(".column-three").paroller({ factor: '0.1', type: 'foreground', direction: 'vertical' });
 
-// var waypoint = $('.section-two-block').waypoint(function(direction) {
-//               if (direction == 'down') {
-//                 $('.section-one-block').addClass('hidden')
-//                 console.log('down')
-//               }
-//               else {
-//                 $('.section-one-block').removeClass('hidden')
-//                 console.log('up')
-//               }},
-//               {
-//                 offset: '-80px'
-//             })
+var waypoint = $('.section-two-block').waypoint(function(direction) {
+              if (direction == 'down') {
+                $('.section-one-block').addClass('hidden')
+                console.log('down')
+              }
+              else {
+                $('.section-one-block').removeClass('hidden')
+                console.log('up')
+              }},
+              {
+                offset: '-80px'
+            })
