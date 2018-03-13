2dc5b4d6298a866b03ca6bf94373eba3947fd166
Colleen O'Reilly <colleen@jelcreative.com>
2018-03-13 10:05:50 -0400

    combo paroller and js

--- script.js
+++ script.js
@@ -1,25 +1,25 @@
 console.log('linked')
 
-$('.two-text').paroller({ factor: '0.3', type: 'foreground', direction: 'vertical' });
+// $('.two-text').paroller({ factor: '0.3', type: 'foreground', direction: 'vertical' });
 
 // $('.column-one').paroller({ factor: '-0.1', type: 'foreground', direction: 'vertical' });
 $('.column-two').paroller({ factor: '0.03', type: 'foreground', direction: 'vertical' });
 
 
-// $(window).on('scroll',function(){
-//     parallaxScroll();
-// });
+$(window).on('scroll',function(){
+    parallaxScroll();
+});
 
 function parallaxScroll(){
     var scrolled = $(window).scrollTop();
     var faster = 0-(scrolled*.15)
-    $('.section-two-block').css('background-position-y', faster+'px');
+    $('.two-text').css('transform', 'translateY('+faster+'px)');
     var one = -scrolled * .08
     var three = scrolled * .08
     // $('.two-text').css('transform','translateY('+faster+'px)');
     // if (scrolled > 1000) {
-  $('.column-one').css('transform','translateY('+one+'px)');
-  $('.column-three').css('transform','translateY('+three+'px)');
+  // $('.column-one').css('transform','translateY('+one+'px)');
+  // $('.column-three').css('transform','translateY('+three+'px)');
 // }
 }
 
